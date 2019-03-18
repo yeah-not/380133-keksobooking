cbbe2772f6c7a37dc448906cba154075e5c82b32
Alex Yeahnotov <yeahhh.not@gmail.com>
2019-03-18 15:49:04 +0200

    Разбил генерацию объявлений на функции

--- js/map.js
+++ js/map.js
@@ -4,24 +4,8 @@ var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
-var NOTICE_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var NOTICE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var NOTICE_CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
-var NOTICE_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var NOTICE_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var NOTICE_Y = [130, 630];
-var NOTICE_X = [0, 500];
-var NOTICES_NUM = 8;
-
-var map = document.querySelector('.map');
-var noticeX = [0, map.offsetWidth]
-
-var notices = [];
-var avatars = [];
-var features = [];
-
-for (var i = 0; i < NOTICES_NUM; i++) {
-  notices[i] = {
+var getNotice = function (avatars) {
+  var notice = {
     author: {
       avatar: ''
     },
@@ -51,33 +35,59 @@ for (var i = 0; i < NOTICES_NUM; i++) {
     avatar = getRandomInt(1, 8);
   } while (avatars.indexOf(avatar) >= 0);
 
-  avatars[i] = avatar;
-  notices[i].author.avatar = 'img/avatars/user0' + avatars[i] + '.png';
+  avatars.push(avatar);
+  notice.author.avatar = 'img/avatars/user0' + avatar + '.png';
 
   // "address": строка, адрес предложения, вида "{{location.x}}, {{location.y}}", например, "600, 350"
-  notices[i].offer.address = notices[i].location.x + ', ' + notices[i].location.y;
+  notice.offer.address = notice.location.x + ', ' + notice.location.y;
 
   // "photos": массив из строк расположенных в произвольном порядке
   var photos = [];
   var photo = '';
 
   for (var j = 0; j < NOTICE_PHOTOS.length; j++) {
-    var k = 0;
     do {
       photo = NOTICE_PHOTOS[getRandomInt(0, NOTICE_PHOTOS.length - 1)];
     } while (photos.indexOf(photo) >= 0);
     photos[j] = photo;
   }
 
-  notices[i].offer.photos = photos;
+  notice.offer.photos = photos;
 
   // "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
+  var features = [];
   features = NOTICE_FEATURES.slice();
   features.length = getRandomInt(1, NOTICE_FEATURES.length);
-  notices[i].offer.features = features;
+  notice.offer.features = features;
 
-  console.log(notices[i]);
+  return notice;
 }
+
+var getNotices = function () {
+  var notices = [];
+  var avatars = [];
+
+  for (var i = 0; i < NOTICES_NUM; i++) {
+    notices[i] = getNotice(avatars);
+  }
+
+  return notices;
+}
+
+var NOTICE_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var NOTICE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var NOTICE_CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
+var NOTICE_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var NOTICE_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var NOTICE_Y = [130, 630];
+var NOTICES_NUM = 8;
+
+var map = document.querySelector('.map');
+var noticeX = [0, map.offsetWidth];
+
+// getNotices();
+
+console.log(getNotices());
 
 map.classList.remove('map--faded');
 
