1bc4f4428b3025315d0af2f21373b594120c867a
Alex Yeahnotov <yeahhh.not@gmail.com>
2019-03-25 10:39:00 +0200

    Оптимизация - именование, комментирование

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Функции
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
@@ -8,21 +9,21 @@ function numberWithSpaces(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
 
-var getPin = function (avatars, data) {
-  var pin = {
+var generateAdvert = function (avatars, data) {
+  var advert = {
     author: {
       avatar: ''
     },
     offer: {
-      title: data.titles[getRandomInt(0, PIN_TITLES.length - 1)],
+      title: data.titles[getRandomInt(0, TITLES.length - 1)],
       address: '',
       price: getRandomInt(1000, 1000000),
-      type: data.types[getRandomInt(0, PIN_TYPES.length - 1)],
+      type: data.types[getRandomInt(0, TYPES.length - 1)],
       rooms: getRandomInt(1, 5),
       guests: getRandomInt(1, 30),
-      checkin: data.checkInOut[getRandomInt(0, PIN_CHECK_IN_OUT.length - 1)],
-      checkout: data.checkInOut[getRandomInt(0, PIN_CHECK_IN_OUT.length - 1)],
-      features: data.features[getRandomInt(0, PIN_FEATURES.length - 1)],
+      checkin: data.checkInOut[getRandomInt(0, CHECK_IN_OUT.length - 1)],
+      checkout: data.checkInOut[getRandomInt(0, CHECK_IN_OUT.length - 1)],
+      features: data.features[getRandomInt(0, FEATURES.length - 1)],
       description: '',
       photos: [],
     },
@@ -40,10 +41,10 @@ var getPin = function (avatars, data) {
   } while (avatars.indexOf(avatar) >= 0);
 
   avatars.push(avatar);
-  pin.author.avatar = 'img/avatars/user0' + avatar + '.png';
+  advert.author.avatar = 'img/avatars/user0' + avatar + '.png';
 
   // "address": строка, адрес предложения, вида "{{location.x}}, {{location.y}}", например, "600, 350"
-  pin.offer.address = pin.location.x + ', ' + pin.location.y;
+  advert.offer.address = advert.location.x + ', ' + advert.location.y;
 
   // "photos": массив из строк расположенных в произвольном порядке
   var photos = [];
@@ -56,70 +57,71 @@ var getPin = function (avatars, data) {
     photos[j] = photo;
   }
 
-  pin.offer.photos = photos;
+  advert.offer.photos = photos;
 
   // "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
   var features = data.features.slice();
   features.length = getRandomInt(1, data.features.length);
-  pin.offer.features = features;
+  advert.offer.features = features;
+
+  return advert;
+};
+
+var generateAdverts = function (amount, data) {
+  var adverts = [];
+  var avatars = [];
+
+  for (var i = 0; i < amount; i++) {
+    adverts[i] = generateAdvert(avatars, data);
+  }
+
+  return adverts;
+};
+
+var renderPin = function (advert, template) {
+  var pin = template.cloneNode(true);
+  var pinImg = pin.querySelector('img');
+
+  pin.style.left = (advert.location.x - WIDTH / 2) + 'px';
+  pin.style.top = advert.location.y + 'px';
+  pinImg.src = advert.author.avatar;
+  pinImg.alt = advert.offer.title;
 
   return pin;
 };
 
-var getPins = function (num, data) {
-  var pins = [];
-  var avatars = [];
-
-  for (var i = 0; i < num; i++) {
-    pins[i] = getPin(avatars, data);
-  }
-
-  return pins;
-};
-
-var renderPin = function (pin, template) {
-  var pinElement = template.cloneNode(true);
-  var pinImgElement = pinElement.querySelector('img');
-
-  pinElement.style = 'left: ' + (pin.location.x - PIN_WIDTH / 2) + 'px; top: ' + pin.location.y + 'px;';
-  pinImgElement.src = pin.author.avatar;
-  pinImgElement.alt = pin.offer.title;
-
-  return pinElement;
-};
-
-var renderPins = function (pins, template) {
+var renderPins = function (adverts, template) {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < pins.length; i++) {
-    fragment.appendChild(renderPin(pins[i], template));
+  for (var i = 0; i < adverts.length; i++) {
+    fragment.appendChild(renderPin(adverts[i], template));
   }
 
   return fragment;
 };
 
-var renderCard = function (pin) {
+var renderCard = function (advert) {
   var card = cardTemplate.cloneNode(true);
-  card.querySelector('.popup__title').textContent = pin.offer.title;
-  card.querySelector('.popup__description').textContent = pin.offer.description;
-  card.querySelector('.popup__avatar').src = pin.author.avatar;
-  card.querySelector('.popup__type').textContent = PIN_TYPES_RUS[PIN_TYPES.indexOf(pin.offer.type)];
-  card.querySelector('.popup__text--address').textContent = pin.offer.address;
-  card.querySelector('.popup__text--time').textContent = 'Заезд после ' + pin.offer.checkin + ', выезд до ' + pin.offer.checkout;
+  card.querySelector('.popup__title').textContent = advert.offer.title;
+  card.querySelector('.popup__description').textContent = advert.offer.description;
+  card.querySelector('.popup__avatar').src = advert.author.avatar;
+  card.querySelector('.popup__type').textContent = TYPES_RUS[TYPES.indexOf(advert.offer.type)];
+  card.querySelector('.popup__text--address').textContent = advert.offer.address;
+  card.querySelector('.popup__text--time').textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
 
   // Русское название типа
-  var pinTypeIndex = PIN_TYPES.indexOf(pin.offer.type);
-  card.querySelector('.popup__type').textContent = PIN_TYPES_RUS[pinTypeIndex];
+  var advertTypeIndex = TYPES.indexOf(advert.offer.type);
+  card.querySelector('.popup__type').textContent = TYPES_RUS[advertTypeIndex];
 
   // Цена с разбивкой на пробелы
-  var price = numberWithSpaces(pin.offer.price, 3);
+  var price = numberWithSpaces(advert.offer.price, 3);
   card.querySelector('.popup__text--price').textContent = price + '₽/ночь';
 
   // Выведите количество гостейи комнат offer.rooms и offer.guests
   // в блок .popup__text--capacityстрокойвида {{offer.rooms}} комнаты для {{offer.guests}} гостей. Например,2 комнаты для 3 гостей.
-  var roomsNum = pin.offer.rooms;
+  var roomsNum = advert.offer.rooms;
   var roomsNumRemainder = roomsNum % 10;
-  var guestsNum = pin.offer.guests;
+  var guestsNum = advert.offer.guests;
   var guestsNumRemainder = guestsNum % 10;
   var roomsLabel = 'комната';
   var guestsLabel = 'гостя';
@@ -137,7 +139,7 @@ var renderCard = function (pin) {
   card.querySelector('.popup__text--capacity').textContent = roomsNum + ' ' + roomsLabel + ' для ' + guestsNum + ' ' + guestsLabel;
 
   // .popup__features - все доступные удобства в объявлении списком
-  var features = pin.offer.features;
+  var features = advert.offer.features;
   var featureTemplate = cardTemplate.querySelector('.popup__feature');
   var featuresList = card.querySelector('.popup__features');
 
@@ -151,7 +153,7 @@ var renderCard = function (pin) {
   }
 
   // .popup__photos - все фотографии из списка offer.photos
-  var photos = pin.offer.photos;
+  var photos = advert.offer.photos;
   var photoTemplate = cardTemplate.querySelector('.popup__photo');
   var photosList = card.querySelector('.popup__photos');
 
@@ -166,41 +168,49 @@ var renderCard = function (pin) {
   return card;
 };
 
-var PIN_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var PIN_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var PIN_TYPES_RUS = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
-var PIN_CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
-var PIN_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PIN_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var PIN_WIDTH = 50;
-var PIN_HEIGHT = 70;
-var PIN_Y = [130, 630];
-var PINS_NUM = 8;
+// Константы
+var ADVERTS_AMOUNT = 8;
+var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var TYPES_RUS = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
+var CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var WIDTH = 50;
+var HEIGHT = 70;
+var Y_RANGE = [130, 630];
 
+// Переменные
 var map = document.querySelector('.map');
-var pinX = [PIN_WIDTH / 2, map.offsetWidth - PIN_WIDTH / 2];
+var xRange = [WIDTH / 2, map.offsetWidth - WIDTH / 2];
 
-var pinsData = {
-  titles: PIN_TITLES,
-  types: PIN_TYPES,
-  typesRus: PIN_TYPES_RUS,
-  checkInOut: PIN_CHECK_IN_OUT,
-  features: PIN_FEATURES,
-  photos: PIN_PHOTOS,
-  dimensions: [PIN_WIDTH, PIN_HEIGHT],
-  xRange: pinX,
-  yRange: PIN_Y
+// Данные
+var advertsData = {
+  titles: TITLES,
+  types: TYPES,
+  typesRus: TYPES_RUS,
+  checkInOut: CHECK_IN_OUT,
+  features: FEATURES,
+  photos: PHOTOS,
+  dimensions: [WIDTH, HEIGHT],
+  xRange: xRange,
+  yRange: Y_RANGE
 };
 
+// DOM-элементы
 var pinsContainer = document.querySelector('.map__pins');
 var filtersContainer = document.querySelector('.map__filters-container');
-var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
-var cardTemplate = document.querySelector('template').content.querySelector('.map__card');
+var template = document.querySelector('template');
+var pinTemplate = template.content.querySelector('.map__pin');
+var cardTemplate = template.content.querySelector('.map__card');
 
-var pins = getPins(PINS_NUM, pinsData);
-pinsContainer.appendChild(renderPins(pins, pinTemplate));
+// ---
+// Старт программы
+// ---
+var adverts = generateAdverts(ADVERTS_AMOUNT, advertsData);
+pinsContainer.appendChild(renderPins(adverts, pinTemplate));
 
-var firstCard = renderCard(pins[0]);
+var firstCard = renderCard(adverts[0]);
 map.insertBefore(firstCard, filtersContainer);
 
 map.classList.remove('map--faded');
