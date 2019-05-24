6368000ce8d834965c6b62f2e2f270e91246b001
Alex Yeahnotov <yeahhh.not@gmail.com>
2019-05-24 09:05:14 +0300

    Превью аватарки - вынес функции. Превью фото жилья - для 1 картинки.

--- js/preview.js
+++ js/preview.js
@@ -10,22 +10,7 @@
 
   // Функции
   // ----------
-
-
-  // Обработчики
-  // ----------
-
-
-  // DOM-элементы
-  // ----------
-  var avatarChooser = document.querySelector('.ad-form-header__input');
-  var avatarPreview = document.querySelector('.ad-form-header__preview img');
-
-  // Старт
-  // ----------
-  avatarChooser.addEventListener('change', function (evt) {
-    var file = evt.target.files[0];
-
+  var choosePreview = function (file, callback) {
     if (file) {
       var fileName = file.name.toLowerCase();
       var isImageFile = IMG_FILE_TYPES.some(function (it) {
@@ -37,12 +22,38 @@
       var fileReader = new FileReader();
 
       fileReader.addEventListener('load', function () {
-        avatarPreview.src = fileReader.result;
+        callback(fileReader.result);
       });
 
       fileReader.readAsDataURL(file);
     }
+  };
 
+  var setPreview = function (previewElement, imdDataURL) {
+    previewElement.src = imdDataURL;
+  };
+
+
+  // Обработчики
+  // ----------
+
+
+  // DOM-элементы
+  // ----------
+  var avatarChooser = document.querySelector('.ad-form-header__input');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+
+  var adPhotoChooser = document.querySelector('.ad-form__input');
+  var adPhotoPreview = document.querySelector('.ad-form__photo img');
+
+  // Старт
+  // ----------
+  avatarChooser.addEventListener('change', function () {
+    choosePreview(avatarChooser.files[0], setPreview.bind(avatarChooser, avatarPreview));
+  });
+
+  adPhotoChooser.addEventListener('change', function () {
+    choosePreview(adPhotoChooser.files[0], setPreview.bind(adPhotoChooser, adPhotoPreview));
   });
 
   // Интерфейс
